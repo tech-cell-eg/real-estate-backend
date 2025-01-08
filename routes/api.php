a92c32a44f148aff711e7b5b6f495bc16c80af16
tech-cell-eg <techcellegy@gmail.com>
2025-01-08 13:22:51 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -40,7 +40,7 @@ use App\Http\Controllers\InspectorProjectsController;
 use App\Http\Controllers\API\Profile\UpdateProfileController;
 use App\Http\Controllers\Company\reviewer\ReviewerCompanyController;
 use App\Http\Controllers\Company\inspector\InspectorCompanyController;
-use App\Http\Controllers\Company\inspector\InspectorProjectController;
+
 use App\Http\Controllers\API\Auth\ResetPassword\ResetPasswordController;
 use App\Http\Controllers\API\Auth\ForgetPassword\ForgetPasswordController;
 use App\Http\Controllers\API\Auth\RegisterClient\RegisterClientIndividualController;
